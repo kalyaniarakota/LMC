79cd19153374effce63ad5777d82444f8dd831eb
Kalyani <arakotakalyani92@gmail.com>
2019-07-24 15:13:08 +0530

    added

--- src/com/Multiplication/Strng.java
+++ src/com/Multiplication/Strng.java
@@ -6,18 +6,6 @@ public class Strng {
 		
 		 String name="kalyani";
 		 //int space =0;
-		for(int i=1;i<=5;i++)
-		{
-		int	space=i*name.length();
-			
-			for(int k=1;k<=space;k++)
-			{
-				System.out.print(" ");
-			}
-			System.out.print(name);
-			System.out.println("");
-			}
-		
-		
+	samba		
 }
 }
